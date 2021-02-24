da3cd8d4ef57bc73240fc5f83c47b8db1cff1335
schun1 <schun@mit.edu>
2021-02-24 01:40:51 -0500

    Final

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ const main = (currentTime) => {
     }
 
     window.requestAnimationFrame(main);
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 500;
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 250;
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) {
         return;
     }
